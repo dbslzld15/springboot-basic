59e6c88acdb771f890e0c7c85036670e33996223
dbslzld15 <dbslzld15@naver.com>
2021-11-15 23:29:28 +0900

    AppConfig 추가를 통해 관심사를 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,14 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
+    /**
+     * MemberServiceImpl은 이제부터 '의존관계' 에 대한 고민은 '외부'(AppConfig)에 맡기고 실행에만 집중하면 된다.
+     **/
+    private final MemberRepository memberRepository;
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    public MemberServiceImpl(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,27 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    /**
+     * 구현체를 직접 생성하는 방법은 DIP를 위반하기때문에 인터페이스에만 의존할 수 있도록 수정
+     * 구체적인 클래스에 대해서 몰라야 함
+     * 해당 클래스는 DiscountPolicy에 대해 FixDiscount가 들어올지, RateDiscount가 들어올지 몰라야함
+     * private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); 보다는
+     * private final DiscountPolicy discountPolicy; 로 가져간 뒤 AppConfig에서 생성자 주입을 통해 설정
+     * */
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
