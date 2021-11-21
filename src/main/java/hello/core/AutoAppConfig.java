f088aaa674a2f2cb7415a4182e7efc47435a2ee0
dbslzld15 <dbslzld15@naver.com>
2021-11-22 00:04:45 +0900

    컴포넌트 스캔 방법 추가

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,18 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan( //컴포넌트 스캔은 이름 그대로 @Component 애노테이션이 붙은 클래스를 스캔해서 스프링 빈으로 등록
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+    /**
+     * 컴포넌트 스캔을 사용하면 @Configuration 이 붙은 설정 정보도 자동으로 등록되기 때문에,
+     * AppConfig, TestConfig 등 앞서 만들어두었던 설정 정보도 함께 등록되고, 실행되어 버린다. 그래서
+     * excludeFilters 를 이용해서 설정정보는 컴포넌트 스캔 대상에서 제외
+     *
+     * **/
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,11 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
     /**
      * MemberServiceImpl은 이제부터 '의존관계' 에 대한 고민은 '외부'(AppConfig)에 맡기고 실행에만 집중하면 된다.
      **/
     private final MemberRepository memberRepository;
 
+    @Autowired //의존관계를 자동으로 주입해준다
     public MemberServiceImpl(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
