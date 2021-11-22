96076a65122617301ddc51dbac29fb171c1760fb
dbslzld15 <dbslzld15@naver.com>
2021-11-22 23:56:05 +0900

    빈 수동주입 vs 자동주입 우선권 코드 추가

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -15,4 +18,12 @@ public class AutoAppConfig {
      * excludeFilters 를 이용해서 설정정보는 컴포넌트 스캔 대상에서 제외
      *
      * **/
+
+//    수동 빈 등록 vs 자동 빈 등록
+//    이 경우 수동 빈 등록이 우선권을 가진다.
+//    (수동 빈이 자동 빈을 오버라이딩 해버린다.)
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
