c2d604f23517d0a528fb1c32e3642f6a1180c13e
dbslzld15 <dbslzld15@naver.com>
2021-11-25 23:45:52 +0900

    빈 등록 초기화, 소멸 메소드 지정 코드 추가

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,16 +1,13 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
 
     private String url;
+
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-//        connect();
-//        call("초기화 연결 메시지");
     }
+
     public void setUrl(String url) {
         this.url = url;
     }
@@ -26,16 +23,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception { // 의존관계 주입이 끝나면 호출됨
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
-        call("초기화 연결 메시지");
+        call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
