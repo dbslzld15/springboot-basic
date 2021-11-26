5978e25d75157004f288f629e5de6b9814740069
dbslzld15 <dbslzld15@naver.com>
2021-11-26 22:22:09 +0900

    ProtoType 스코프 테스트 코드 추가

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,43 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2); //완전히 다른 스프링 빈이 생성되고 초기화도 2번 실행됨
+
+        ac.close(); //프로토타입 빈은 스프링 컨테이너가 종료될때 @PreDestory 종료 메서드가 실행되지 않음
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() { //
+            System.out.println("PrototypeBean.destroy");
+        }
+
+    }
+}
