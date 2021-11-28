e98e592bc92be6dd62c5f445669b87d563ff4291
dbslzld15 <dbslzld15@naver.com>
2021-11-28 18:55:17 +0900

    Provider 사용 예제 추가

--- src/test/java/hello/core/scope/PrototypeProviderTest2.java
+++ src/test/java/hello/core/scope/PrototypeProviderTest2.java
@@ -2,16 +2,22 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class PrototypeProviderTest2 {
-
+    /**
+     * 참고: 실무에서 자바 표준인 JSR-330 Provider를 사용할 것인지, 아니면 스프링이 제공하는
+     * ObjectProvider를 사용할 것인지 고민이 될 것이다. ObjectProvider는 DL을 위한 편의 기능을 많이
+     * 제공해주고 스프링 외에 별도의 의존관계 추가가 필요 없기 때문에 편리하다.
+     * 만약(정말 그럴일은 거의없겠지만) 코드를 스프링이 아닌 다른 컨테이너에서도 사용할 수 있어야 한다면
+     * JSR-330 Provider를 사용해야한다.
+     * **/
     @Test
     void prototypeFind() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
@@ -28,15 +34,17 @@ public class PrototypeProviderTest2 {
     @Scope("singleton")
     static class ClientBean {
 
-        private final ObjectProvider<PrototypeBean> prototypeBeanProvider; //Dependency Lookup
+        //private final ObjectProvider<PrototypeBean> prototypeBeanProvider; //Object Provider을 사용해 Dependency Lookup
+        private final Provider<PrototypeBean> prototypeBeanProvider; //JSR303 Provider을 사용해 DL
 
         @Autowired
-        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+        public ClientBean(Provider<PrototypeBean> prototypeBeanProvider) {
             this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
